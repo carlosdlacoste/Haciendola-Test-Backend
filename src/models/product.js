3d4cc24b8ef8d984e22ffa91d81950b3a4777d99
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-14 09:33:54 -0400

    Creado modelo products con los campos proporcionados en la tabla excel

--- src/models/product.js
+++ src/models/product.js
@@ -1,27 +1,43 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../utils/db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/db');
 
-// const Product = sequelize.define('Product', {
-//     sku: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     nombre: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     descripcion: {
-//         type: DataTypes.TEXT,
-//         allowNull: true
-//     },
-//     precio: {
-//         type: DataTypes.FLOAT,
-//         allowNull: false
-//     },
-//     stock: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false
-//     }
-// });
+const Product = sequelize.define('Product', {
+    handle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    sku: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    grams: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    compare_price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    barcode: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }
+});
 
-// module.exports = Product;
+module.exports = Product;
